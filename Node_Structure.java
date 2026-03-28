12ed85141cb46654c999978b5d72d893096c2622
Shadow Hunter <debabir82@gmail.com>
2026-03-28 13:45:09 +0600

    Add Node class and main method for linked list demo
    Implement a simple Node class and a main method to demonstrate its usage.

---
+++ Node_Structure.java
@@ -0,0 +1,24 @@
+class Node{
+    int data;
+    Node next;
+}
+public class NodeOfLinkedList {
+    public static void main(String[] args) {
+        Node a = new Node();
+        a.data = 1;
+        Node b = new Node();
+        b.data = 2;
+        Node c = new Node();
+        c.data = 3 ;
+        System.out.println(a.data);
+        System.out.println(b.data);
+        System.out.println(c.data);
+        System.out.println(a.next);
+        System.out.println(b.next);
+        System.out.println(c.next);
+
+
+
+
+    }
+}
